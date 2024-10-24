12167c3cd84d15a3b35703cd74987397b57694bb
camilosan98 <camilo.santanilla00@usc.edu.co>
2024-10-23 21:55:38 -0500

    Actualizando API del editor de texto

--- Blog__Net/Models/PostLike.cs
+++ Blog__Net/Models/PostLike.cs
@@ -7,11 +7,14 @@ namespace Blog__Net.Models
 {
     public class PostLike
     {
+
         public Guid Id { get; set; }
 
-        public Guid PostId { get; set; }
+        public int PostId { get; set; }
 
-        public Guid UserId { get; set; }
+        public int? UserId { get; set; }
+
+        public DateTime LikeDate { get; set; }
 
     }
 }
